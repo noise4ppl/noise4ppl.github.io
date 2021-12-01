2070d9e06b7184c0a64077e65ff9229954e34428
noise4ppl <94338522+noise4ppl@users.noreply.github.com>
2021-12-01 03:10:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+const scrollBtn = document.querySelector('.scroll-to-top')
+
+const refreshButtonVisibility = () => {
+  if (document.documentElement.scrollTop < 3200){
+    scrollBtn.style.display = "none";
+  } else {
+    scrollBtn.style.display = "block";
+  }
+};
+refreshButtonVisibility();
+
+console.log(scrollBtn); 
+scrollBtn.addEventListener('click', () => {
+document.documentElement.scrollTop = 0;
+});
+
+document.addEventListener('scroll', (e) => {
+refreshButtonVisibility();
+});
